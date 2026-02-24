44ecad5abe3e88090a1287aef83b953d6d713354
poolevent <garasivantoni@gmail.com>
2026-02-24 19:05:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,106 @@
-const supabaseClient = supabase.createClient(
-"https://rrjxsdxaholwhdzidpvx.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyanhzZHhhaG9sd2hkemlkcHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzcwNTcsImV4cCI6MjA4NzQxMzA1N30.VIcs9IqSpZUAw43gYYQ4RWWb29dBIk422BuQzWeyA6k"
-);
-
-async function getMatches(){
+async function loadLive(){
 const { data } = await supabaseClient
 .from("matches")
 .select("*")
-.order("table_number",{ascending:true});
+.eq("status","live");
 
-return data || [];
+renderMatches(data);
 }
 
-async function updateScore(id,score1,score2){
+async function loadUpcoming(){
+const { data } = await supabaseClient
+.from("matches")
+.select("*")
+.eq("status","upcoming");
+
+renderMatches(data);
+}
+
+async function loadFinished(){
+const { data } = await supabaseClient
+.from("matches")
+.select("*")
+.eq("status","finished");
+
+renderMatches(data);
+}
+
+function renderMatches(matches){
+const container=document.getElementById("content");
+container.innerHTML="";
+
+matches.forEach(match=>{
+container.innerHTML+=`
+<div class="match">
+${match.player1} 
+<span class="score">${match.score1}</span>
+vs
+<span class="score">${match.score2}</span>
+${match.player2}
+</div>
+`;
+});
+}
+
+async function loadAdminMatches(){
+const { data } = await supabaseClient
+.from("matches")
+.select("*");
+
+const div=document.getElementById("matchesAdmin");
+div.innerHTML="";
+
+data.forEach(match=>{
+div.innerHTML+=`
+<div class="match">
+${match.player1} ${match.score1}
+<button onclick="addScore(${match.id},1)">+</button>
+
+${match.player2} ${match.score2}
+<button onclick="addScore(${match.id},2)">+</button>
+</div>
+`;
+});
+}
+
+async function addScore(id,player){
+let match = await supabaseClient
+.from("matches")
+.select("*")
+.eq("id",id)
+.single();
+
+let s1=match.data.score1;
+let s2=match.data.score2;
+
+if(player==1) s1++;
+if(player==2) s2++;
+
 await supabaseClient
 .from("matches")
-.update({score1,score2})
-.eq("id",id)
+.update({
+score1:s1,
+score2:s2
+})
+.eq("id",id);
+
+loadAdminMatches();
+}
+
+async function createEvent(){
+const name=document.getElementById("eventNameInput").value;
+const venue=document.getElementById("venueInput").value;
+const race=document.getElementById("raceInput").value;
+const format=document.getElementById("formatInput").value;
+
+await supabaseClient
+.from("events")
+.insert({
+name:name,
+venue:venue,
+race_to:race,
+format:format
+});
+
+alert("Event Created");
 }
